58c4d840edc33980fe048984331160aeb05beeba
bcround <sbc1142@gmail.com>
2021-02-08 09:55:13 +0900

    feat: Fix analog clock time

--- analog-clock/app.js
+++ analog-clock/app.js
@@ -12,18 +12,18 @@ const second = setInterval(() => {
     $second.style.transform = `rotate(${secDeg}deg)`;
   }
   secDeg += 6;
-}, 1);
+}, 1000);
 
 const minute = setInterval(() => {
   if (minDeg !== 0) {
     $minute.style.transform = `rotate(${minDeg}deg)`;
   }
   if (secDeg % 360 === 0) minDeg += 6;
-}, 1)
+}, 1000)
 
 const hour = setInterval(() => {
   if (hourDeg !== 0) {
     $hour.style.transform = `rotate(${hourDeg}deg)`;
   }
   hourDeg += 30 / 3600;
-}, 1)
+}, 1000)
